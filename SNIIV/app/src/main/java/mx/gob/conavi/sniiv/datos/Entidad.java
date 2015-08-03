7ef463b9838515377e30879b6b71e3a34edfe9bb
enrique7mc <enrique7mc@gmail.com>
2015-08-03 16:24:05 -0500

    Añade Enum para representar las entidades

---
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/datos/Entidad.java
@@ -0,0 +1,42 @@
+package mx.gob.conavi.sniiv.datos;
+
+/**
+ * Created by admin on 03/08/15.
+ */
+public enum Entidad {
+    NAC,
+    AGS
+    ,BC
+    ,BCS
+    ,CAMP
+    ,COAH
+    ,COL
+    ,CHIS
+    ,CHIH
+    ,DF
+    ,DUR
+    ,GTO
+    ,GRO
+    ,HGO
+    ,JAL
+    ,MEX
+    ,MICH
+    ,MOR
+    ,NAY
+    ,NL
+    ,OAX
+    ,PUE
+    ,QRO
+    ,QROO
+    ,SLP
+    ,SIN
+    ,SON
+    ,TAB
+    ,TAMPS
+    ,TLAX
+    ,VER
+    ,YUC
+    ,ZAC
+    ,pd
+    ,SDIN
+}
