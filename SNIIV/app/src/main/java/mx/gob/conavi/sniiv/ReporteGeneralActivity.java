3f4000d857a6822786edea3e1a5f0506cfc851db
enrique7mc <enrique7mc@gmail.com>
2015-08-04 17:40:30 -0500

    Adds connectivity permission

---
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/ReporteGeneralActivity.java
@@ -0,0 +1,13 @@
+package mx.gob.conavi.sniiv;
+
+import android.support.v4.app.Fragment;
+
+import mx.gob.conavi.sniiv.activities.SingleFragmentActivity;
+import mx.gob.conavi.sniiv.fragments.ReporteGeneralFragment;
+
+public class ReporteGeneralActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new ReporteGeneralFragment();
+    }
+}
