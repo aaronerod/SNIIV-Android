da9fa269db87229a8e99e3c47591f9ba1d99f462
enrique7mc <enrique7mc@gmail.com>
2015-08-07 13:13:53 -0500

    Actualiza la versión de base de datos
    Añade catálogos a las columnas de financiamientos y subsidios

--- SNIIV/app/src/main/java/mx/gob/conavi/sniiv/sqlite/AdminSQLiteOpenHelper.java
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/sqlite/AdminSQLiteOpenHelper.java
@@ -8,11 +8,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import mx.gob.conavi.sniiv.modelos.AvanceObra;
+import mx.gob.conavi.sniiv.modelos.PCU;
 import mx.gob.conavi.sniiv.modelos.ReporteGeneral;
+import mx.gob.conavi.sniiv.modelos.TipoVivienda;
+import mx.gob.conavi.sniiv.modelos.ValorVivienda;
 
 class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME = "sniiv.db";
 
     public AdminSQLiteOpenHelper(Context context ) {
@@ -80,23 +84,64 @@ class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
                         ")");
 
         db.execSQL(
-                "CREATE TABLE Financiamiento(" +
-                        "cve_ent INTEGER" +
-                        ",organismo TEXT" +
-                        ",destino TEXT" +
-                        ",agrupacion TEXT" +
-                        ",acciones INTEGER" +
-                        ",monto REAL" +
+                "CREATE TABLE TipoEntidadEjecutora(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                        ",descripcion TEXT" +
+                        ")");
+
+        db.execSQL(
+                "CREATE TABLE Modalidad(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                        ",descripcion TEXT" +
                         ")");
 
         db.execSQL(
                 "CREATE TABLE Subsidio(" +
                         "cve_ent INTEGER" +
-                        ",tipo_ee TEXT" +
-                        ",modalidad TEXT" +
+                        ",tipo_ee_id INTEGER" +
+                        ",modalidad_id INTEGER" +
                         ",acciones INTEGER" +
                         ",monto REAL" +
+                        ",FOREIGN KEY(tipo_ee_id) REFERENCES TipoEntidadEjecutora(id)" +
+                        ",FOREIGN KEY(modalidad_id) REFERENCES Modalidad(id)" +
                         ")");
+
+        db.execSQL(
+                "CREATE TABLE Organismo(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                        ",descripcion TEXT" +
+                        ")");
+
+        db.execSQL(
+                "CREATE TABLE Destino(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                        ",descripcion TEXT" +
+                        ")");
+
+        db.execSQL(
+                "CREATE TABLE Agrupacion(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                        ",descripcion TEXT" +
+                        ")");
+
+        db.execSQL(
+                "CREATE TABLE Financiamiento(" +
+                        "cve_ent INTEGER" +
+                        ",organismo_id INTEGER" +
+                        ",destino_id INTEGER" +
+                        ",agrupacion_id INTEGER" +
+                        ",acciones INTEGER" +
+                        ",monto REAL" +
+                        ",FOREIGN KEY(organismo_id) REFERENCES Organismo(id)" +
+                        ",FOREIGN KEY(destino_id) REFERENCES Destino(id)" +
+                        ",FOREIGN KEY(agrupacion_id) REFERENCES Agrupacion(id)" +
+                        ")");
+
+        insertTipoEntidadEjecutora(db);
+        insertModalidad(db);
+        insertOrganismo(db);
+        insertDestino(db);
+        insertAgrupacion(db);
     }
 
     @Override
@@ -108,13 +153,67 @@ class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int versionAnte, int versionNue) {
         db.execSQL("DROP TABLE IF EXISTS " + ReporteGeneral.TABLE);
         db.execSQL("DROP TABLE IF EXISTS Fechas");
-        db.execSQL("DROP TABLE IF EXISTS AvanceObra");
-        db.execSQL("DROP TABLE IF EXISTS PCU");
-        db.execSQL("DROP TABLE IF EXISTS TipoVivienda");
-        db.execSQL("DROP TABLE IF EXISTS ValorVivienda");
-        db.execSQL("DROP TABLE IF EXISTS Financiamiento");
+        db.execSQL("DROP TABLE IF EXISTS " + AvanceObra.TABLE );
+        db.execSQL("DROP TABLE IF EXISTS " + PCU.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + TipoVivienda.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + ValorVivienda.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS EntidadEjecutora");
+        db.execSQL("DROP TABLE IF EXISTS Modalidad");
         db.execSQL("DROP TABLE IF EXISTS Subsidio");
+        db.execSQL("DROP TABLE IF EXISTS Organismo");
+        db.execSQL("DROP TABLE IF EXISTS Destino");
+        db.execSQL("DROP TABLE IF EXISTS Agrupacion");
+        db.execSQL("DROP TABLE IF EXISTS Financiamiento");
         init(db);
+    }
+
+    private void insertTipoEntidadEjecutora(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO TipoEntidadEjecutora(descripcion) VALUES ('INFONAVIT')");
+        db.execSQL("INSERT INTO TipoEntidadEjecutora(descripcion) VALUES ('FOVISSSTE')");
+        db.execSQL("INSERT INTO TipoEntidadEjecutora(descripcion) VALUES ('FUERZAS ARMADAS')");
+        db.execSQL("INSERT INTO TipoEntidadEjecutora(descripcion) VALUES ('OREVIS')");
+        db.execSQL("INSERT INTO TipoEntidadEjecutora(descripcion) VALUES ('PROGRAMA DE MEJORAMIENTO " +
+                "DE UNIDAD HABITACIONAL')");
+        db.execSQL("INSERT INTO TipoEntidadEjecutora(descripcion) VALUES ('OTRAS ENTIDADES EJECUTORAS')");
+    }
+
+    private void insertModalidad(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO Modalidad(descripcion) VALUES ('Nueva')");
+        db.execSQL("INSERT INTO Modalidad(descripcion) VALUES ('Usada')");
+        db.execSQL("INSERT INTO Modalidad(descripcion) VALUES ('Autoproduccion')");
+        db.execSQL("INSERT INTO Modalidad(descripcion) VALUES ('Orevis')");
+        db.execSQL("INSERT INTO Modalidad(descripcion) VALUES ('Lotes con Servicio')");
+        db.execSQL("INSERT INTO Modalidad(descripcion) VALUES ('Otros')");
+    }
+
+    private void insertOrganismo(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('FOVISSSTE')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('INFONAVIT')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('SHF (FONDEO)')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('BANJERCITO')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('BANCA (CNBV)')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('CONAVI')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('FONHAPO')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('Indivi')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('COVEG')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('IMEVIS')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('IVEM')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('ISSFAM')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('CFE')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('PEMEX')");
+        db.execSQL("INSERT INTO Organismo(descripcion) VALUES ('HABITAT MEXICO')");
+    }
+
+    private void insertDestino(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO Destino(descripcion) VALUES ('Viviendas Nuevas')");
+        db.execSQL("INSERT INTO Destino(descripcion) VALUES ('Viviendas Usadas')");
+        db.execSQL("INSERT INTO Destino(descripcion) VALUES ('Mejoramientos')");
+        db.execSQL("INSERT INTO Destino(descripcion) VALUES ('Otros Programas')");
+    }
+
+    private void insertAgrupacion(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO Agrupacion(descripcion) VALUES ('Cofinanciamientos y Subsidios')");
+        db.execSQL("INSERT INTO Agrupacion(descripcion) VALUES ('Crédito Individual')");
     }
 }
 
