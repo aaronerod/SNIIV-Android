c9d40e4e7df4646958a3cc538a750d3f67dc6ac0
enrique7mc <enrique7mc@gmail.com>
2015-08-11 13:11:02 -0500

    Completa modelo, datos, parsing y repository de Financiamiento

---
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/modelos/ConsultaFinanciamiento.java
@@ -0,0 +1,63 @@
+package mx.gob.conavi.sniiv.modelos;
+
+/**
+ * Created by admin on 10/08/15.
+ */
+public class ConsultaFinanciamiento {
+    private FinanciamientoResultado viviendasNuevas;
+    private FinanciamientoResultado viviendasUsadas;
+    private FinanciamientoResultado mejoramientos;
+    private FinanciamientoResultado otrosProgramas;
+    private Consulta total;
+
+    public FinanciamientoResultado getViviendasNuevas() {
+        return viviendasNuevas;
+    }
+
+    public void setViviendasNuevas(FinanciamientoResultado viviendasNuevas) {
+        this.viviendasNuevas = viviendasNuevas;
+    }
+
+    public FinanciamientoResultado getViviendasUsadas() {
+        return viviendasUsadas;
+    }
+
+    public void setViviendasUsadas(FinanciamientoResultado viviendasUsadas) {
+        this.viviendasUsadas = viviendasUsadas;
+    }
+
+    public FinanciamientoResultado getMejoramientos() {
+        return mejoramientos;
+    }
+
+    public void setMejoramientos(FinanciamientoResultado mejoramientos) {
+        this.mejoramientos = mejoramientos;
+    }
+
+    public FinanciamientoResultado getOtrosProgramas() {
+        return otrosProgramas;
+    }
+
+    public void setOtrosProgramas(FinanciamientoResultado otrosProgramas) {
+        this.otrosProgramas = otrosProgramas;
+    }
+
+    public Consulta getTotal() {
+        return total;
+    }
+
+    public void setTotal(Consulta total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "ConsultaFinanciamiento{" +
+                "viviendasNuevas=" + viviendasNuevas +
+                ", viviendasUsadas=" + viviendasUsadas +
+                ", mejoramientos=" + mejoramientos +
+                ", otrosProgramas=" + otrosProgramas +
+                ", total=" + total +
+                '}';
+    }
+}
