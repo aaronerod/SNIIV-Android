2ad54d45e09c03e8c6adeabda8dc37b5bd0dab48
enrique7mc <enrique7mc@gmail.com>
2015-08-27 16:22:07 -0500

    Modifica MainActivity
    La lectura de fechas ahora se hará en esta actividad

--- SNIIV/app/src/main/java/mx/gob/conavi/sniiv/activities/MainActivity.java
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package mx.gob.conavi.sniiv.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,12 +15,21 @@ import java.util.ArrayList;
 
 import mx.gob.conavi.sniiv.R;
 import mx.gob.conavi.sniiv.Utils.Utils;
+import mx.gob.conavi.sniiv.datos.DatosReporteGeneral;
+import mx.gob.conavi.sniiv.modelos.Fechas;
+import mx.gob.conavi.sniiv.modelos.ReporteGeneral;
+import mx.gob.conavi.sniiv.parsing.ParseFechas;
+import mx.gob.conavi.sniiv.parsing.ParseFechasWeb;
+import mx.gob.conavi.sniiv.parsing.ParseReporteGeneral;
+import mx.gob.conavi.sniiv.sqlite.FechasRepository;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
     ListView lvwMenu;
     ArrayAdapter<String> arrayAdapter;
     ArrayList<String> listMenu;
+    protected FechasRepository fechasRepository;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +46,25 @@ public class MainActivity extends AppCompatActivity {
                 changeActivity(position);
             }
         });
+
+        fechasRepository = new FechasRepository(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (Utils.isNetworkAvailable(this)) {
+            new AsyncTaskRunner().execute();
+        }
+    }
+
+    protected void cargarDatosFechas() {
+        Log.v(TAG, "carga fechas");
+        ParseFechasWeb parseFechasWeb = new ParseFechasWeb();
+        Fechas[] fechas = parseFechasWeb.getDatos();
+        fechasRepository.deleteAll();
+        fechasRepository.saveAll(fechas);
     }
 
     public void changeActivity(int position) {
@@ -49,5 +80,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         startActivity(intent);
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                cargarDatosFechas();
+            } catch (Exception e) {
+                Log.v(TAG, "Error obteniendo fechas " + e.getMessage());
+            }
+
+            return null;
+        }
     }
 }
