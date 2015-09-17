1d20dcd9ce66dea7bffd61fe4a578699636ba0b9
enrique7mc <enrique7mc@gmail.com>
2015-09-17 16:46:57 -0500

    Cambia layout para contener fragment

--- SNIIV/app/src/main/java/mx/gob/conavi/sniiv/fragments/BaseFragment.java
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/fragments/BaseFragment.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ import mx.gob.conavi.sniiv.Utils.Utils;
 import mx.gob.conavi.sniiv.activities.SniivApplication;
 import mx.gob.conavi.sniiv.datos.Datos;
 import mx.gob.conavi.sniiv.modelos.Fechas;
-import mx.gob.conavi.sniiv.parsing.ParseFechas;
 import mx.gob.conavi.sniiv.sqlite.FechasRepository;
 import mx.gob.conavi.sniiv.sqlite.Repository;
 
