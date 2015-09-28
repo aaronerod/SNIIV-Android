beb6ec8716de81d4724dfe2181946f473a64e176
enrique7mc <enrique7mc@gmail.com>
2015-09-28 16:26:17 -0500

    Añade clase de datos para EvolucionFinanciamientos

---
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/datos/DatosEvolucionFinanciamiento.java
@@ -0,0 +1,36 @@
+package mx.gob.conavi.sniiv.datos;
+
+import android.content.Context;
+
+import mx.gob.conavi.sniiv.modelos.EvolucionFinanciamiento;
+import mx.gob.conavi.sniiv.sqlite.EvolucionFinanciamientoRepository;
+
+/**
+ * Created by octavio.munguia on 24/09/2015.
+ */
+public class DatosEvolucionFinanciamiento extends Datos<EvolucionFinanciamiento> {
+    EvolucionFinanciamiento[] datos;
+
+    public DatosEvolucionFinanciamiento(Context context, EvolucionFinanciamiento[] datos) {
+        super(datos);
+        repository = new EvolucionFinanciamientoRepository(context);
+    }
+
+    @Override
+    public EvolucionFinanciamiento consultaNacional() {
+        return ((EvolucionFinanciamientoRepository)repository).consultaNacional();
+    }
+
+    @Override
+    public EvolucionFinanciamiento consultaEntidad(int entidad) {
+        EvolucionFinanciamiento resultado = null;
+        for (int i = 0; i < datos.length; i++) {
+            if (i == entidad) {
+                resultado = datos[i];
+                break;
+            }
+        }
+
+        return resultado;
+    }
+}
