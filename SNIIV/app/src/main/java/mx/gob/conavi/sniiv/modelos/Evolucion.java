74223428560003d648c6cb6125541128b204a0ff
enrique7mc <enrique7mc@gmail.com>
2015-10-05 16:13:37 -0500

    Refactoriza nombres

--- SNIIV/app/src/main/java/mx/gob/conavi/sniiv/modelos/Evolucion.java
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/modelos/Evolucion.java
@@ -15,17 +15,18 @@ import mx.gob.conavi.sniiv.parsing.ParseFechas;
  * Created by octavio.munguia on 24/09/2015.
  */
 public class Evolucion {
-    private static String TAG = ParseFechas.class.getSimpleName();
+    private static String TAG = Evolucion.class.getSimpleName();
     public static final String FINANCIAMIENTO = "Financiamientos";
     public static final String SUBSIDIO = "Subsidios";
+    public static final String REGISTRO_VIVIENDA = "Registro Vivienda";
 
-    Map<String, EvolucionFinanciamientoResultado> periodos;
+    Map<String, EvolucionResultado> periodos;
 
     public Evolucion() {
         periodos = new TreeMap<>();
     }
 
-    public Evolucion(Map<String, EvolucionFinanciamientoResultado> periodos) {
+    public Evolucion(Map<String, EvolucionResultado> periodos) {
         this.periodos = periodos;
     }
 
@@ -36,16 +37,16 @@ public class Evolucion {
         while(keys.hasNext()) {
             String key = (String)keys.next();
             JSONArray array = (JSONArray) json.get(key);
-            EvolucionFinanciamientoResultado efr = new EvolucionFinanciamientoResultado(array);
+            EvolucionResultado efr = new EvolucionResultado(array);
             periodos.put(key, efr);
         }
     }
 
-    public Map<String, EvolucionFinanciamientoResultado> getPeriodos() {
+    public Map<String, EvolucionResultado> getPeriodos() {
         return periodos;
     }
 
-    public void setPeriodos(Map<String, EvolucionFinanciamientoResultado> periodos) {
+    public void setPeriodos(Map<String, EvolucionResultado> periodos) {
         this.periodos = periodos;
     }
 
@@ -57,7 +58,7 @@ public class Evolucion {
     public ArrayList<double[]> getYValuesAcciones() {
         ArrayList<double[]> yValues = new ArrayList<>();
         for (String key : periodos.keySet()) {
-            EvolucionFinanciamientoResultado resultado = periodos.get(key);
+            EvolucionResultado resultado = periodos.get(key);
             Consulta[] meses = resultado.getMeses();
             double[] values = new double[meses.length];
             for (int i = 0; i < meses.length; i++) {
@@ -73,7 +74,7 @@ public class Evolucion {
     public ArrayList<double[]> getYValuesMontos() {
         ArrayList<double[]> yValues = new ArrayList<>();
         for (String key : periodos.keySet()) {
-            EvolucionFinanciamientoResultado resultado = periodos.get(key);
+            EvolucionResultado resultado = periodos.get(key);
             Consulta[] meses = resultado.getMeses();
             double[] values = new double[meses.length];
             for (int i = 0; i < meses.length; i++) {
