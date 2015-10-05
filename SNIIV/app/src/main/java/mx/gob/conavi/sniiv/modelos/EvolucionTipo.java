2cdfde204910aff9fd4a7e7fe7afc8fd36e71f47
enrique7mc <enrique7mc@gmail.com>
2015-10-05 11:48:05 -0500

    Añade EvolucionTipo
    Esta enum representa dos tipos de Evolucion: financiamiento o subsidio

---
+++ SNIIV/app/src/main/java/mx/gob/conavi/sniiv/modelos/EvolucionTipo.java
@@ -0,0 +1,29 @@
+package mx.gob.conavi.sniiv.modelos;
+
+/**
+ * Created by octavio.munguia on 02/10/2015.
+ */
+public enum EvolucionTipo {
+    FINANCIAMIENTOS(0),
+    SUBSIDIOS(1);
+
+    private final int value;
+    private EvolucionTipo(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static EvolucionTipo fromInteger(int x) {
+        switch(x) {
+            case 0:
+                return FINANCIAMIENTOS;
+            case 1:
+                return SUBSIDIOS;
+        }
+
+        return null;
+    }
+}
